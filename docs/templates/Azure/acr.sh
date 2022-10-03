176a8e1120732f61a03bedd08cb6f3f8e24b3ab8
Frank Oprel <frank.oprel@vodafoneziggo.com>
2022-10-03 17:04:26 +0200

    Changed var names

--- docs/templates/Azure/acr.sh
+++ docs/templates/Azure/acr.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Set environment variables
-DOCKER_IMAGE={{ cfg["docker_image_name"] }}
+DOCKER_IMAGE_NAME={{ cfg["docker_image_name"] }}
 AZURE_CONTAINER_REGISTRY_NAME={{ cfg["container_registry_name"] }}
 AZURE_CLIENT_ID={{ cnx["AZURE_CLIENT_ID"] }}
 AZURE_CLIENT_SECRET={{ cnx["AZURE_CLIENT_SECRET"] }}
-AZURE_TENANT_ID={{ cnx["AZURE_CLIENT_SECRET"] }}
+AZURE_TENANT_ID={{ cnx["AZURE_TENANT_ID"] }}
+AZURE_RESOURCE_GROUP_NAME={{ cfg["resource_group_name"] }}
 
 # Login to Azure
 az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET --tenant $AZURE_TENANT_ID
@@ -13,11 +14,18 @@ az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET --tenan
 # Login to Azure Container Registry
 az acr login --name $AZURE_CONTAINER_REGISTRY_NAME
 
-# Build Docker image
-docker build -t $DOCKER_IMAGE .
+# Build Docker image locally
+docker build -t $DOCKER_IMAGE_NAME .
 
-# Tag Docker image to create alias
-docker tag $DOCKER_IMAGE:latest $AZURE_CONTAINER_REGISTRY_NAME.azurecr.io/$AZURE_CONTAINER_REGISTRY_NAME:latest
+# Tag Docker image and create alias
+docker tag $DOCKER_IMAGE_NAME:latest $AZURE_CONTAINER_REGISTRY_NAME.azurecr.io/$DOCKER_IMAGE_NAME:latest
 
-# Push Docker image to Azure registry
-docker push $AZURE_CONTAINER_REGISTRY_NAME.azurecr.io/$AZURE_CONTAINER_REGISTRY_NAME:latest
+# Push Docker image to Azure Container Registry
+docker push $AZURE_CONTAINER_REGISTRY_NAME.azurecr.io/$DOCKER_IMAGE_NAME:latest
+
+# TODO: Authenticate
+# Create Docker container on Azure
+az container create --resource-group $AZURE_RESOURCE_GROUP_NAME --name $DOCKER_CONTAINER_NAME --image $AZURE_CONTAINER_REGISTRY_NAME.azurecr.io/$DOCKER_IMAGE_NAME
+
+# Start Docker container on Azure
+az container start --resource-group $AZURE_RESOURCE_GROUP_NAME --name $DOCKER_CONTAINER_NAME --no-wait
