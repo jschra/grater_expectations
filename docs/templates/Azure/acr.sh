be4ab783677145e3d0be314a5f1e550f37ca542a
jschra <j.c.m.schra@gmail.com>
2022-10-10 17:29:55 +0200

    Adjusted template for ACR (Azure)

--- docs/templates/Azure/acr.sh
+++ docs/templates/Azure/acr.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 
-# Set environment variables
-#TODO: refactor --> take env vars out, implemennt proper templating and injection of values (except for az login creds)
-DOCKER_IMAGE_NAME={{ cfg["docker_image_name"] }}
-AZURE_CONTAINER_REGISTRY_NAME={{ cfg["container_registry_name"] }}
-AZURE_RESOURCE_GROUP_NAME={{ cfg["resource_group_name"] }}
-
 # Login to Azure
-az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET --tenant $AZURE_TENANT_ID
+# -- Note that environment variables containing service principal credentials are required
+# -- to log in. Ensure these are set in the calling terminal before running this script
+az login --service-principal -u $ARM_CLIENT_ID -p $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID
 
 # Create a registry on ACR
-az acr create --resource-group $AZURE_RESOURCE_GROUP_NAME --name $AZURE_CONTAINER_REGISTRY_NAME --sku Basic --admin-enabled true
+az acr create --resource-group {{cfg["resource_group_name"]}} --name {{cfg["container_registry_name"]}} --sku Basic --admin-enabled true
 
 # Login to Azure Container Registry
-az acr login --name $AZURE_CONTAINER_REGISTRY_NAME
+az acr login --name {{cfg["container_registry_name"]}}
 
 # Build Docker image locally
-docker build -t $DOCKER_IMAGE_NAME .
+docker build -t {{cfg["docker_image_name"]}} .
 
 # Tag Docker image and create alias
-docker tag $DOCKER_IMAGE_NAME:latest $AZURE_CONTAINER_REGISTRY_NAME.azurecr.io/$DOCKER_IMAGE_NAME:latest
+docker tag {{cfg["docker_image_name"]}}:latest {{cfg["container_registry_name"]}}.azurecr.io/{{cfg["docker_image_name"]}}:latest
 
 # Push Docker image to Azure Container Registry
-docker push $AZURE_CONTAINER_REGISTRY_NAME.azurecr.io/$DOCKER_IMAGE_NAME:latest
+docker push {{cfg["container_registry_name"]}}.azurecr.io/{{cfg["docker_image_name"]}}:latest
