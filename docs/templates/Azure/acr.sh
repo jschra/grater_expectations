ac9f0e8cf1f11c5f349ff257fd9b1b24139e83c9
jschra <j.c.m.schra@gmail.com>
2022-10-13 08:57:45 +0200

    Adjusted acr.sh template to reflect changes in Dockerfile

--- docs/templates/Azure/acr.sh
+++ docs/templates/Azure/acr.sh
@@ -11,8 +11,13 @@ az acr create --resource-group {{cfg["resource_group_name"]}} --name {{cfg["cont
 # Login to Azure Container Registry
 az acr login --name {{cfg["container_registry_name"]}}
 
-# Build Docker image locally
-docker build -t {{cfg["docker_image_name"]}} .
+# Build Docker image locally, pass Service Principal credentials
+docker build -t {{cfg["docker_image_name"]}} \
+--build-arg AZURE_CLIENT_SECRET=${ARM_CLIENT_SECRET} \
+--build-arg AZURE_TENANT_ID=${ARM_TENANT_ID} \
+--build-arg AZURE_SUBSCRIPTION_ID=${ARM_SUBSCRIPTION_ID} \
+--build-arg AZURE_TENANT_ID=${ARM_TENANT_ID} \
+.
 
 # Tag Docker image and create alias
 docker tag {{cfg["docker_image_name"]}}:latest {{cfg["container_registry_name"]}}.azurecr.io/{{cfg["docker_image_name"]}}:latest
