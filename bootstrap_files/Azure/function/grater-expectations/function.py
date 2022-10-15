3d967947a375d463d932f481e18279b3e350c30d
jschra <j.c.m.schra@gmail.com>
2022-10-15 11:57:51 +0200

    Updated functions

--- bootstrap_files/Azure/function/grater-expectations/function.py
+++ bootstrap_files/Azure/function/grater-expectations/function.py
@@ -15,7 +15,6 @@ from supporting_functions import (
     setup_logging,
     get_connection_string,
 )
-from supporting_functions import load_csv_from_container as load_data
 
 # -- General imports
 import logging
@@ -36,40 +35,28 @@ PATH_GE_CONFIG = PATH_PROJECT_ROOT + "great_expectations"
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info("Python HTTP trigger function processed a request.")
 
-    # -- 0. Load parameters from configuration file and event
+    # -- 0. Load parameters from configuration file, initialize context
     test_config = TestingConfiguration(PATH_PROJECT_CONFIG)
     test_config.load_config()
-
-    # -- 1. Parse request params, derive asset_name from path_to_file
-    request_params = dict(req.params)
-    path_to_file = request_params["path_file"]
-    asset_name = path_to_file.split("/")[-1]
-
-    # -- 2. Authenticate on Azure and initilize blob service
-    credentials = DefaultAzureCredential()
-    storage_client = StorageManagementClient(
-        credentials, os.environ.get("AZURE_SUBSCRIPTION_ID")
-    )
-    connection_string = get_connection_string(storage_client, test_config)
-
-    blob_service_client = BlobServiceClient.from_connection_string(
-        conn_str=connection_string
-    )
-
-    # -- 3. Initialize GE DataContext
     context = ge.data_context.DataContext(context_root_dir=PATH_GE_CONFIG)
 
-    # -- 4. Load data using load_data and set a batch identifier that can be used in
-    #       the RuntimeBatchRequest to identify the current batch being run
-    df_batch = load_data(
-        blob_service_client=blob_service_client,
-        container_name=test_config.data_container_name,
-        path_csv=path_to_file,
-    )
-    #       Extract batch_identifier by pulling date from filename (year_month)
-    batch_identifier = re.search(r"\d{4}\-\d{2}", asset_name)[0]
+    # -- 1. Parse request params
+    request_params = dict(req.params)
 
-    # -- 5. Generate batch request to run validations using a checkpoint
+    # -- 2. Load data
+    ### PUT YOUR DATA LOADING LOGIC HERE
+    # To validate a data batch, data must be loaded to subsequently validate
+    # Accordingly, in the lines below custom logic is required to load
+    # a dataset as a pandas DataFrame in the df_batch argument
+    # To make the RuntimeBatchRequest complete, which is used by GE for running
+    # validations, asset_name and batch_identifier are required
+    # NOTE: in order for this to properly run, an expectation suite must have been
+    # previously generated!
+    df_batch = load_data()
+    asset_name = "ASSET NAME OR LOGIC TO GENERATE IT GOES HERE"
+    batch_identifier = "BATCH IDENTIFIER OR LOGIC TO GENERATE IT GOES HERE"
+
+    # -- 3. Generate batch request to run validations using a checkpoint
     batch_request = RuntimeBatchRequest(
         datasource_name="runtime_data",
         data_connector_name="runtime_data_connector",
@@ -78,19 +65,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         batch_identifiers={"batch_identifier": batch_identifier},
     )
 
-    # -- 6. Set dynamic evaluation parameters
-    #       Here, evaluation parameters are provided at runtime. They are hard-coded
-    #       for simplicity, but note that you could develop your own logic to for
-    #       example derive testing values with data from last month to test the data
-    #       of this month against. The code below just shows you how this can be done
-    MIN_MAX_PASSENGER_COUNT = 5
-    MAX_MAX_PASSENGER_COUNT = 8
-    dict_evaluation_parameters = {
-        "min_max_passenger_count": MIN_MAX_PASSENGER_COUNT,
-        "max_max_passenger_count": MAX_MAX_PASSENGER_COUNT,
-    }
-
-    # -- 7. Run validations
+    # -- 4. Run validations
     #       Below, the checkpoint generated in the expectation_suite.ipynb is being
     #       called, passing the currently loaded dataset as batch request to run the
     #       expectations against. To accomodate for the dynamic evaluation parameters,
@@ -99,10 +74,9 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     results = context.run_checkpoint(
         checkpoint_name=f"{test_config.checkpoint_name}",
         validations=[{"batch_request": batch_request}],
-        evaluation_parameters=dict_evaluation_parameters,
     )
 
-    # -- 8. Evaluate results from running the expectations on the current batch of data,
+    # -- 5. Evaluate results from running the expectations on the current batch of data,
     #       return statuscode 200 if successfull
     success = evaluate_ge_results(results)
 
