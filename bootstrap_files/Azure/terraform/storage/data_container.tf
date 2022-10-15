a93118b33c1640c8c06ec7ebdb8a07347b083937
jschra <j.c.m.schra@gmail.com>
2022-10-15 11:53:17 +0200

    Updated expectation_suite.ipynb for Azure

---
+++ bootstrap_files/Azure/terraform/storage/data_container.tf
@@ -0,0 +1,5 @@
+# resource "azurerm_storage_container" "data_container" {
+#   name                  = var.data_container
+#   storage_account_name  = azurerm_storage_account.this.name
+#   container_access_type = "private"
+# }
